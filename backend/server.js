bc00d65f6935c21789e862b4903ff3c46da14f45
Amit07Ak <118081686+Amit07Ak@users.noreply.github.com>
2025-12-28 11:27:18 +0530

    Day 2: Setup Express server with basic route

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
-console.log("App is just Starting.....")
+const express = require("express");
+
+const app = express();
+
+// middleware
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("MERN Todo API is running");
+});
+
+// server
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
