004560a1bd47ad87e1620d57b08edb8e581d5368
Amit07Ak <118081686+Amit07Ak@users.noreply.github.com>
2025-12-29 18:45:21 +0530

    Mongo db setup completed with connection.

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 // middleware
 app.use(express.json());
 
+// DB connection
+mongoose
+  .connect("PASTE_YOUR_MONGODB_URI_HERE")
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
 // test route
 app.get("/", (req, res) => {
-  res.send("MERN Todo API is running");
+  res.send("MERN Todo API with DB connected");
 });
 
 // server
